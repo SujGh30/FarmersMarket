bf23a9e915cfc69e030769da0ecf38882200adc5
sujathaghatty <sujathaghatty@msn.com>
2017-10-01 11:02:36 -0700

    Initial commit

---
+++ FarmersMarket/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmersMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate an object
+            var vegetable = new Vegetables();
+
+            vegetable.Name = "Beans";
+            vegetable.Price = 2;
+            vegetable.Quantity = 1;
+
+            Console.WriteLine($"VN: {vegetable.Name}, price:{ vegetable.Price:C}, pounds:{vegetable.Quantity}" );
+
+            var vegetable2 = new Vegetables();
+            Console.WriteLine($"VN: {vegetable2.Name}, price:{ vegetable2.Price:C}, pounds:{vegetable2.Quantity}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ FarmersMarket/Vegetables.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmersMarket
+{
+    class Vegetables
+
+    {
+        private static float Totalcost = 0;
+        private static int Itemscount = 0;
+       
+        // Properties 
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public float Quantity { get; set; }    
+
+        //Constructor  
+
+        public Vegetables()
+        {
+            
+            Itemscount += Vegetables.Itemscount;
+            
+        }
+            
+
+    }
+}
